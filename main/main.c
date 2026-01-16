5aba576f9ff7252eb0e0e3bcea26db8df3fae698
Somya Dwivedi <somya.dwivedi@yahoo.com>
2026-01-16 13:40:25 -0500

    Add project files.

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display_array(int a[], int size);
+void swap(int* x, int* y);
+
+
+int main() {
+    
+    int arr[] = { 12, 234, 5234, 120, 53, 12, 34, 531, 1,4,135, 13 ,2, 182, 412, 123 };
+
+        
+
+    return 0;
+}
